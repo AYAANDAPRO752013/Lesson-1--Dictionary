ec2e80aca0437002cfd9f7449e6324c18a462d05
AYAANDAPRO752013 <sumit985aj@gmail.com>
2024-09-17 18:54:48 +0200

    Add files via upload

---
+++ Lesson 1- Dictionary.py
@@ -0,0 +1,10 @@
+food=["fries","rice","pizza"]
+print(food)
+
+Ayaan={"Name":"Ayaan",
+        "Grade":6,
+        "School":"KFG",
+        "Age":11,
+        "Hobby":"Badminton"}
+
+print(Ayaan)        
